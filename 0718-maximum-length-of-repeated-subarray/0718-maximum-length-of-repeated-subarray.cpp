28a07fb45917a92e630932f07ccaec95af35d5e2
Piyush Singh <95350374+Darth-InVader15@users.noreply.github.com>
2024-01-15 03:41:12 +0530

    Time: 233 ms (29.96%), Space: 85.8 MB (59.16%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,35 +1,25 @@
 class Solution {
-    // int ans = -1;
-    int solve(int ind1, int ind2,int &cnt, vector<int> &arr1, vector<int> &arr2, vector<vector<int>> &dp)
-    {
-        if(ind1 == arr1.size() || ind2 == arr2.size())  return 0;
-
-        if(dp[ind1][ind2] != -1)    return dp[ind1][ind2];
-
-        int take = 0;
-
-        if(arr1[ind1] == arr2[ind2])
-           take = 1 + solve(ind1+1,ind2+1,cnt,arr1,arr2,dp);
-        
-        //skip1 
-        solve(ind1+1,ind2,cnt,arr1,arr2,dp);
-        solve(ind1,ind2+1,cnt,arr1,arr2,dp);
-
-        cnt = max(cnt,take);
-
-        return dp[ind1][ind2] = take;
-
-
-    }
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        
         int n = nums1.size();
         int m = nums2.size();
         int ans = 0;
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        solve(0,0,ans,nums1,nums2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(nums1[i-1] == nums2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+
+                else
+                    dp[i][j] = 0;
+                
+                ans = max(ans, dp[i][j]);
+            }
+        }
         return ans;
         
     }
